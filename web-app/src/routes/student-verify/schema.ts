2e4258e7f1bc002280ef9e164f6c69e64124eb62
WolverinDEV <git@did.science>
2026-01-10 02:10:47 +0100

    feat(web-app): stricter email username verification

--- web-app/src/routes/student-verify/schema.ts
+++ web-app/src/routes/student-verify/schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const kFormSchema = z.object({
-    email: z.string().min(1).max(64),
+    email: z.string().min(1).max(64).refine(data => /[a-z0-9\.\-+]/i.test(data)),
 });
 
 export type FormSchema = typeof kFormSchema;
