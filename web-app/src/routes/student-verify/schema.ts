f7f46b978a8b3ecbfb95863c52d3b479ced7867f
WolverinDEV <git@did.science>
2026-01-10 02:55:10 +0100

    feat(web-app): improved email error feedback texts

--- web-app/src/routes/student-verify/schema.ts
+++ web-app/src/routes/student-verify/schema.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
 
 export const kFormSchema = z.object({
-    email: z.string().min(1).max(64).refine(data => /^[a-z0-9\.\-+]+$/ig.test(data), { error: "Ungültige Zeichen" }),
+    email: z.string()
+        .min(4, { error: "Deine E-Mail sollte min. 4 Zeichen lang sein" })
+        .max(64, { error: "Deine E-Mail sollte max. 64 Zeichen lang sein" })
+        .refine(data => /^[a-z0-9\.\-+]+$/ig.test(data), { error: "Deine E-Mail enthält ungültige Zeichen" }),
 });
 
 export type FormSchema = typeof kFormSchema;
